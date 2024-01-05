2bd93cb476dedb393f4ce5f31a33f36ba81fe4e9
Marius Smarandescu <marius.smarandescu@randstaddigital.com>
2024-01-05 10:34:50 +0200

    add verification for exit code of compilation

--- compille.sh
+++ compille.sh
@@ -5,28 +5,38 @@ rm -rf out/ | true
 mkdir -p out/obj
 
 echo "Compilation started:"
+exitCode=$?
 for f in $(find -type f -name "*.cpp")
 do 
     $(g++ -Wall -g -c $f -o "out/obj/$(basename -- ${f%.*}.o)")
-    echo $f
+    exitCode=$?
+    echo "  $f"
+    if [ $exitCode -gt 0 ]
+    then 
+        break
+    fi
 done
-echo "Compilation successful!"
-echo
 
-echo "Linking started:"
-obj=""
-for o in $(ls "out/obj")
-do
-    obj="$obj out/obj/$o"
-    echo "Linking file: "$o
-done
-echo "Linking end!"
+if [ $exitCode -gt 0 ]
+then 
+    echo "Compilation failed! exit code:$exitCode"
+else
+    echo "Compilation successful!"
+    echo
 
-$(g++ -o out/main.exe $obj)
-echo
-echo "Project build successful!"
+    echo "Linking started:"
+    obj=""
+    for o in $(ls "out/obj")
+    do
+        obj="$obj out/obj/$o"
+        echo "  Linking file: "$o
+    done
+    $(g++ -o out/main.exe $obj)
+    echo "Linking end!"
+    echo "Project build successful!"
 
-echo
-echo Program output:
-out/main.exe
-echo
+    echo
+    echo "==================Program output:=================="
+    out/main.exe
+    echo "====================Program end===================="
+fi
